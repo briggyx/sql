cc2df4a2d17f248cb83cc4841a81d24d4f6210d5
brigitte <brigitte.xyan@gmail.com>
2024-09-16 16:46:21 -0400

    finished string manipulation queries

--- 02_activities/homework/homework_4.sql
+++ 02_activities/homework/homework_4.sql
@@ -75,11 +75,23 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-
+SELECT 
+    product_id,
+    product_name,
+    TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1)) AS description
+FROM product
+WHERE INSTR(product_name, '-') > 0;
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
-
+SELECT 
+    product_id,
+    product_name,
+    TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1)) AS description,
+    product_size
+FROM product
+WHERE INSTR(product_name, '-') > 0
+  AND product_size REGEXP '[0-9]';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
